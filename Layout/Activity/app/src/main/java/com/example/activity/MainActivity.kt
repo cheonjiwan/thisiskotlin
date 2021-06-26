0967ff885c738705a1c02486c3000e437b404099
cheonjiwan <giyan1000@naver.com>
2021-06-26 20:22:42 +0900

    intent

---
+++ Layout/Activity/app/src/main/java/com/example/activity/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.activity
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.activity.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val intent = Intent(this,SubActivity::class.java)
+        intent.putExtra("from1","Hello Bundle")
+        intent.putExtra("from2",2021)
+        binding.btnStart.setOnClickListener { startActivityForResult(intent,99) }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode == RESULT_OK){
+            when(requestCode){
+                99 ->{
+                    val message = data?.getStringExtra("returnValue")
+                    Toast.makeText(this,message,Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+}
