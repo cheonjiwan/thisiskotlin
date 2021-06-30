3a7433fa22dec0a0f6d806d50ff96d83d2d28a57
cheonjiwan <giyan1000@naver.com>
2021-07-01 01:21:28 +0900

    Coroutine - 이미지 다운로드

---
+++ Android_Programming/Coroutine/app/src/main/java/com/example/coroutine/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.coroutine
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.coroutine.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.net.URL
+import kotlin.coroutines.CoroutineContext
+
+suspend fun loadImage(imageUrl: String): Bitmap {
+    val url = URL(imageUrl)
+    val stream = url.openStream()
+    return BitmapFactory.decodeStream(stream)
+}
+
+class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.run {
+            buttonDownload.setOnClickListener {
+                CoroutineScope(Dispatchers.Main).launch {
+                    binding.progress.visibility = View.VISIBLE
+                    val url = binding.editUrl.text.toString()
+
+                    val bitmap = withContext(Dispatchers.IO){
+                        loadImage(url)
+                    }
+
+                    binding.imagePriview.setImageBitmap(bitmap)
+                    binding.progress.visibility = View.GONE
+                }
+            }
+        }
+    }
+}
