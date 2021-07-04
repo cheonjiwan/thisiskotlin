08696f8c0ed4746c1f2dacc0cb13edd51d20ff98
cheonjiwan <giyan1000@naver.com>
2021-07-04 22:21:31 +0900

    네트워크 - Retrofit

---
+++ Android_Programming/NetworkRetrofit/app/src/main/java/com/example/networkretrofit/CustomAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.networkretrofit
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.networkretrofit.databinding.ItemRecyclerBinding
+
+class CustomAdapter: RecyclerView.Adapter<Holder>() {
+
+    var userList: Repository? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val binding = ItemRecyclerBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return Holder(binding)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        var user = userList?.get(position)
+        holder.setUser(user)
+    }
+
+    override fun getItemCount(): Int {
+        return userList?.size?:0
+    }
+}
+
+class Holder(val binding: ItemRecyclerBinding): RecyclerView.ViewHolder(binding.root){
+    fun setUser(user: RepositoryItem?){
+        user?.let {
+            binding.textName.text = it.name
+            binding.textId.text = it.node_id
+            Glide.with(binding.imageAvatar).load(it.owner.avatar_url).into(binding.imageAvatar)
+        }
+    }
+}
